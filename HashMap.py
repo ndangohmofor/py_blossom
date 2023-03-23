89e08c3c118965ebdc6de4331a60e20f9bf4a828
ndangohmofor <ndangohmofor@gmail.com>
2023-03-23 08:01:46 -0400

    Added the Node and LinkedList classes and updated the Hashmap to use them

--- HashMap.py
+++ HashMap.py
@@ -1,7 +1,11 @@
+from linkedList import LinkedList
+from node import Node
+
+
 class HashMap:
     def __init__(self, size=0):
         self.array_size = size
-        self.array = [None for i in range(size)]
+        self.array = [LinkedList() for i in range(size)]
 
     def hash(self, key):
         return sum(key.encode())
@@ -12,7 +16,12 @@ class HashMap:
     def assign(self, key, value):
         hash_code = self.hash(key)
         array_index = self.compressor(hash_code)
-        self.array[array_index] = [key, value]
+        payload = Node([key, value])
+        list_at_index=self.array[array_index]
+        for i in list_at_index:
+            if key == i[0]:
+                i[1]=value
+        list_at_index.insert(payload)
 
     def retrieve(self, key):
         hash_code = self.hash(key)
